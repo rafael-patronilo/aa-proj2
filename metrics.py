322cb7f2b36d5cd28ac5e9190f6d256d8b014604
Rafael Patronilo <r.patronilo@campus.fct.unl.pt>
2022-12-04 17:01:00 +0000

    finished most of the metrics
    Co-authored-by: João Padrão <Padraao@users.noreply.github.com>

---
+++ metrics.py
@@ -0,0 +1,30 @@
+from sklearn.metrics.cluster import pair_confusion_matrix
+import numpy as np
+
+def purity_score(pred_labels, true_labels):
+    clusters = set(pred_labels)
+    classes = list(set(true_labels))
+    score = 0
+    for cluster in clusters:
+        idx = pred_labels == cluster
+        score += max(np.sum(true_labels[idx]==cls) for cls in classes)
+    return score / pred_labels.shape[0]
+        
+
+def calculate_metrics(pred_labels, true_labels):
+    purity = purity_score(pred_labels, true_labels)
+    
+    confusion = pair_confusion_matrix(true_labels, pred_labels)
+    true_neg = confusion[0,0] 
+    false_pos = confusion[0,1]
+    false_neg = confusion[1,0]
+    true_pos = confusion[1,1]
+    total_pairs = pred_labels.shape[0] ** 2 - pred_labels.shape[0]
+    
+    precision = true_pos / (true_pos + false_pos)
+    recall = true_pos / (true_pos + false_neg)
+    f1 = 2 * (precision * recall) / (precision + recall)
+    rand = (true_pos + true_neg) / total_pairs
+    
+    return purity, precision, recall, f1, rand
+
--- tp2.py
+++ tp2.py
@@ -1,15 +1,18 @@
 from tp2_aux import images_as_matrix, report_clusters, report_clusters_hierarchical
+from metrics import calculate_metrics
 from sklearn.decomposition import PCA, KernelPCA
 from sklearn.manifold import Isomap
 from sklearn.cluster import AgglomerativeClustering, SpectralClustering, KMeans
-from sklearn.metrics import precision_score, recall_score, f1_score, rand_score
+
 from sklearn.preprocessing import Normalizer
 from datetime import datetime
 import matplotlib.pyplot as plt
 import numpy as np
 
+
 N_FEATURES = 6
 K_RANGE = range(2, 9)
+D_RANGE = np.arange(0.1, 6.1, 0.1)
 
 data = images_as_matrix()
 
@@ -37,20 +40,55 @@ extracted = np.hstack(extracted)
 extracted = Normalizer().fit_transform(extracted)
 
 clustering_methods = (
-    lambda k : AgglomerativeClustering(n_clusters=k),
-    lambda k : SpectralClustering(assign_labels="cluster_qr", n_clusters=k),
-    lambda k : KMeans(n_clusters=k)
+    ("Agglomerative", lambda k : AgglomerativeClustering(n_clusters=k)),
+    ("Spectral", lambda k : SpectralClustering(assign_labels="cluster_qr", n_clusters=k)),
+    ("KMeans", lambda k : KMeans(n_clusters=k))
 )
 
-clusterings = (
-    clustering(k).fit_predict(data) for clustering in clustering_methods for k in K_RANGE
+true_labels = np.loadtxt("labels.txt", delimiter=",")[:,1]
+labeled = true_labels > 0
+true_labels = true_labels[labeled]
+
+def plot_metrics(clustering_methods, varying_parameter, parameter_name):
+    purity_plot = []
+    precision_plot = []
+    recall_plot = []
+    f1_plot = []
+    rand_plot = []
+    for method_name, method in clustering_methods:
+        purity_plot.append((method_name, []))
+        precision_plot.append((method_name, []))
+        recall_plot.append((method_name, []))
+        f1_plot.append((method_name, []))
+        rand_plot.append((method_name, []))
+        for x in varying_parameter:
+            pred = method(x).fit_predict(extracted)[labeled]
+            purity, precision, recall, f1, rand = calculate_metrics(pred, true_labels)
+            purity_plot[-1][1].append(purity)
+            precision_plot[-1][1].append(precision)
+            recall_plot[-1][1].append(recall)
+            f1_plot[-1][1].append(f1)
+            rand_plot[-1][1].append(rand)
+    _, ax = plt.subplots(2, 3, sharex=True)
+    
+    def plot_metric(i, metric_name, plots):
+        legend = []
+        for method_name, value in plots:
+            legend.append(ax.flat[i].plot(varying_parameter, value, label=method_name)[0])
+        ax.flat[i].legend(handles=legend)
+        ax.flat[i].set(xlabel = parameter_name)
+        ax.flat[i].set_title(metric_name)
+        ax.flat[i].label_outer()
+    
+    plot_metric(0, "purity", purity_plot)
+    plot_metric(1, "precision", precision_plot)
+    plot_metric(2, "recall", recall_plot)
+    plot_metric(3, "f1", f1_plot)
+    plot_metric(4,"rand index", rand_plot)
+
+plot_metrics(clustering_methods, K_RANGE, "k clusters")
+aglomerative_distance = (
+    ("Agglomerative", lambda d : AgglomerativeClustering(n_clusters=None, distance_threshold=d)),
 )
-
-true_labels = None
-
-def plot_metric(name, metric):
-    for pred in clusterings:
-        plt.plot(K_RANGE, [metric(true_labels, labels) for labels in pred])
-
-for metric in [precision_score, recall_score, f1_score, rand_score]:
-    plot_metric("", metric)
+plot_metrics(aglomerative_distance, D_RANGE, "distance")
+plt.show()
