649259745324ae38452c4973d6e5dbf5f7aa8c07
Rafael Patronilo <r.patronilo@campus.fct.unl.pt>
2022-12-22 11:36:21 +0000

    things

--- tp2.py
+++ tp2.py
@@ -3,28 +3,34 @@ from metrics import calculate_metrics
 from sklearn.decomposition import PCA, KernelPCA
 from sklearn.manifold import Isomap
 from sklearn.cluster import AgglomerativeClustering, SpectralClustering, KMeans
-
-from sklearn.preprocessing import Normalizer
+from sklearn.feature_selection import f_classif, SelectKBest
+from sklearn.preprocessing import Normalizer, StandardScaler
 from datetime import datetime
 import matplotlib.pyplot as plt
 import numpy as np
 
 
-N_FEATURES = 6
+N_FEATURES_EXTRACT = 6
+N_FEATURES_SELECT = 6
 K_RANGE = range(2, 9)
 D_RANGE = np.arange(0.1, 6.1, 0.1)
 
 data = images_as_matrix()
+id_to_label = np.loadtxt("labels.txt", delimiter=",")
+true_labels = id_to_label[:,1]
+labeled = true_labels > 0
+true_labels = true_labels[labeled]
 
 def extract_features(method):
     method.fit(data)
     return method.transform(data)
 
 extraction_methods = (
-    PCA(n_components=N_FEATURES), 
-    KernelPCA(kernel="rbf", n_components=N_FEATURES), 
-    Isomap(n_components=N_FEATURES)
+    PCA(n_components=N_FEATURES_EXTRACT), 
+    KernelPCA(kernel="rbf", n_components=N_FEATURES_EXTRACT), 
+    Isomap(n_components=N_FEATURES_EXTRACT)
     )
+extraction_method_names = ["PCA", "kPCA", "Isomap"]
 
 print("Extracting Features")
 start = datetime.now()
@@ -37,6 +43,14 @@ for method, m in zip(extraction_methods, extracted):
     print(f"\t{m[0,:]}")
 extracted = np.hstack(extracted)
 
+print("Selecting best features")
+selector = SelectKBest(f_classif, k = N_FEATURES_SELECT).fit(extracted[labeled], true_labels)
+print(selector.get_feature_names_out(
+        [f"{method}{i}" for i in range(N_FEATURES_EXTRACT) for method in extraction_method_names]
+    )
+)
+extracted = selector.transform(extracted)
+
 extracted = Normalizer().fit_transform(extracted)
 
 clustering_methods = (
@@ -45,11 +59,6 @@ clustering_methods = (
     ("KMeans", lambda k : KMeans(n_clusters=k))
 )
 
-id_to_label = np.loadtxt("labels.txt", delimiter=",")
-true_labels = id_to_label[:,1]
-labeled = true_labels > 0
-true_labels = true_labels[labeled]
-
 def plot_metrics(clustering_methods, varying_parameter, parameter_name):
     purity_plot = []
     precision_plot = []
@@ -57,20 +66,20 @@ def plot_metrics(clustering_methods, varying_parameter, parameter_name):
     f1_plot = []
     rand_plot = []
     kmeans_loss_plot = []
-    all_clusterings = []
+    all_clusterings = {}
     for method_name, method in clustering_methods:
         purity_plot.append((method_name, []))
         precision_plot.append((method_name, []))
         recall_plot.append((method_name, []))
         f1_plot.append((method_name, []))
         rand_plot.append((method_name, []))
-        all_clusterings.append((method_name, []))
+        all_clusterings[method_name] = []
         if method_name == "KMeans":
             kmeans_loss_plot.append((method_name, []))
         for x in varying_parameter:
             method_obj = method(x)
             pred = method_obj.fit_predict(extracted)[labeled]
-            all_clusterings[-1][1].append(pred)
+            all_clusterings[method_name].append(pred)
             purity, precision, recall, f1, rand = calculate_metrics(pred, true_labels)
             purity_plot[-1][1].append(purity)
             precision_plot[-1][1].append(precision)
@@ -79,7 +88,7 @@ def plot_metrics(clustering_methods, varying_parameter, parameter_name):
             rand_plot[-1][1].append(rand)
             if method_name == "KMeans":
                 kmeans_loss_plot[-1][1].append(method_obj.inertia_)
-    _, ax = plt.subplots(2, 3, sharex=True)
+    fig, ax = plt.subplots(2, 3, sharex=True)
     
     def plot_metric(i, metric_name, plots):
         legend = []
@@ -88,8 +97,7 @@ def plot_metrics(clustering_methods, varying_parameter, parameter_name):
         ax.flat[i].legend(handles=legend)
         ax.flat[i].set(xlabel = parameter_name)
         ax.flat[i].set_title(metric_name)
-        ax.flat[i].label_outer()
-    
+    fig.tight_layout()
     plot_metric(0, "purity", purity_plot)
     plot_metric(1, "precision", precision_plot)
     plot_metric(2, "recall", recall_plot)
@@ -105,7 +113,15 @@ aglomerative_distance = (
 )
 plot_metrics(aglomerative_distance, D_RANGE, "distance")
 
+def report(name, x):
+    report_clusters(id_to_label[labeled][:,0], all_clusterings[name][x-K_RANGE.start], f"{name}-{x}.html")
+
+report("Agglomerative", 3)
+report("Spectral", 3)
+report("KMeans", 3)
+"""
 for method_name, clusterings in all_clusterings:
         for x, clustering in zip(K_RANGE, clusterings):
             report_clusters(id_to_label[labeled][:,0], clustering, f"{method_name}-{x}.html")
+"""
 plt.show()
